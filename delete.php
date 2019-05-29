f054667cc137cbded5de86ba3a3292aec4f8f98c
Lucas <lucas@fxsistemas.com.br>
2019-05-29 08:53:06 -0300

    Fixed string;

--- delete.php
+++ delete.php
@@ -41,7 +41,7 @@
 
             <form class="form-horizontal" action="delete.php" method="post">
                 <input type="hidden" name="id" value="<?php echo $id;?>"/>
-                <p class="alert alert-error" >Are you sure to delete?</p>
+                <p class="alert alert-error">Are you sure to delete?</p>
                 <div class="form-actions">
                     <button type="submit" class="btn btn-danger">Yes</button>
                     <a class="btn" href="mainPage.php">No</a>
